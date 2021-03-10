cd6162ebb204688aac8775e72643c8a2ae7debde
priya521 <priya521.j@gmail.com>
2021-03-10 21:03:37 +0000

    adding play.js file

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require('net');
+//const { IP, PORT } = require('./constants');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: '50542'
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server.");
+    conn.write('Name: Priya');
+  });
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+console.log('Connecting ...');
+connect();
+//module.exports = { connect };
