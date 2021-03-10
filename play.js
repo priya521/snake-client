f0a154063e71b854dd022d4e81b332e776c64ae6
priya521 <priya521.j@gmail.com>
2021-03-10 23:05:42 +0000

    adding move commands

--- play.js
+++ play.js
@@ -1,28 +1,20 @@
-//const net = require('./client/');
-//const { IP, PORT } = require('./constants');
 
-// const connect = function() {
-//   const conn = net.createConnection({
-//     host: '135.23.222.131',
-//     port: '50542'
-//   });
-//   // interpret incoming data as text
-//   conn.setEncoding('utf8');
-
-//   conn.on('connect', () => {
-//     console.log("Successfully connected to game server.");
-//     conn.write('Name: Priya');
-//   });
-
-//   conn.on('data', (data) => {
-//     console.log(data);
-//   });
-
-//   return conn;
-// };
-// console.log('Connecting ...');
-// connect();
-//module.exports = { connect };
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+if (key === 'w') {
+  connection.write('Move: up');
+}
+
+if (key === 'a') {
+  connection.write('Move: left');
+}
+
+if (key === 's') {
+  connection.write('Move: down');
+}
+
+if (key === 'd') {
+  connection.write('Move: right');
+}
